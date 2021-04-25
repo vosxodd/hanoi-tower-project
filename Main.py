74abb30471c66c74887a69a7b0b127e39371a590
vosxodd <73089079+vosxodd@users.noreply.github.com>
2021-04-25 22:28:41 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,9 @@
-from functools import lru_cache
-
 def move_discs(num, from_peg, to_peg, temp_peg):
     if num > 0:
         move_discs(num - 1, from_peg, temp_peg, to_peg)
         print('Переложить диск с', from_peg, 'на', to_peg)
         move_discs(num - 1, temp_peg, to_peg, from_peg)
 
-
-@lru_cache(maxsize=None)
 def main():
     num_discs = int(input('Сколько дисков на первом стержне? '))
     from_peg = 1
